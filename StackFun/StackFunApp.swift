1b91d2444370c6f19c98c2a3ee0aa054eb50a75d
trulli-kenneth <123408292+trulli-kenneth@users.noreply.github.com>
2023-03-15 13:53:59 -0400

    Initial Commit

---
+++ StackFun/StackFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackFunApp.swift
+//  StackFun
+//
+//  Created by Kenneth Adams on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct StackFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
